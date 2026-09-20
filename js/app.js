a63bc28a016e9eee559beb79995080a6962b00ce
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-20 20:37:38 +0200

    v0.10.3: Service Worker aktualisieren

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 (async function bootstrap(){
   state = await loadState();
   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js?v=0.10.2')
+    navigator.serviceWorker.register('./sw.js?v=0.10.3')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
