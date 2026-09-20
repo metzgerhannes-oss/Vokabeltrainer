1fa22afb6ce9065f21ac33f868149e0ef852d2e3
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-20 20:53:47 +0200

    v0.10.4: App-Shell aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION='0.10.3';
+const APP_VERSION='0.10.4';
 const RESOURCE_REVISION='1';
 const SHELL_CACHE=`vokabeltrainer-shell-v${APP_VERSION}`;
 const RESOURCE_CACHE=`vokabeltrainer-resources-v${RESOURCE_REVISION}`;
@@ -10,9 +10,9 @@ const LEGACY_CACHE_PREFIX='vokabeltrainer-v';
 const LEGACY_APP_BASES=['/JohannasGartenwelt/vokabeltrainer/'];
 
 const ASSETS=[
-  './','./index.html','./css/app.css?v=0.10.3','./js/core.js?v=0.10.3','./js/help.js?v=0.10.3','./js/library.js?v=0.10.3','./js/storage.js?v=0.10.3',
-  './js/model.js?v=0.10.3','./js/learning.js?v=0.10.3','./js/translation.js?v=0.10.3',
-  './js/io.js?v=0.10.3','./js/ui.js?v=0.10.3','./js/library-ui.js?v=0.10.3','./js/focus-ui.js?v=0.10.3','./js/app.js?v=0.10.3','./manifest.webmanifest',
+  './','./index.html','./css/app.css?v=0.10.4','./js/core.js?v=0.10.4','./js/help.js?v=0.10.4','./js/library.js?v=0.10.4','./js/storage.js?v=0.10.4',
+  './js/model.js?v=0.10.4','./js/learning.js?v=0.10.4','./js/translation.js?v=0.10.4',
+  './js/io.js?v=0.10.4','./js/ui.js?v=0.10.4','./js/library-ui.js?v=0.10.4','./js/focus-ui.js?v=0.10.4','./js/app.js?v=0.10.4','./manifest.webmanifest',
   './assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
 ];
 
