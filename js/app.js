f09256b818cbda093f4afc955cb126423e01b1bf
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 20:05:35 +0200

    Release: 0.18.5 für bibliotheksgestützte Camden-OCR

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.18.4')
+    navigator.serviceWorker.register('./sw.js?v=0.18.5')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
