6ce8a56a176923003a3df4be2ddc9884c7e4bd8a
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 06:45:47 +0200

    PWA: nach Service-Worker-Update einmal sauber neu laden

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 (async function bootstrap(){
   state = await loadState();
   if('serviceWorker' in navigator){
+    const hadController=!!navigator.serviceWorker.controller;
+    let updateReloading=false;
+    navigator.serviceWorker.addEventListener('controllerchange',()=>{
+      if(!hadController||updateReloading)return;
+      const key='vokabeltrainer_sw_reload_'+VERSION;
+      if(sessionStorage.getItem(key))return;
+      updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
+    });
     navigator.serviceWorker.register('./sw.js?v=0.12.1')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
