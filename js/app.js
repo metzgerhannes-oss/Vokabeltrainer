8a403530ac3ab2e694287a4dee68864738784007
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 07:43:29 +0200

    Release v0.13.1: Guided Shell Cache erneuern

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.13.0')
+    navigator.serviceWorker.register('./sw.js?v=0.13.1')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '0.13.0';
+const VERSION = '0.13.1';
 const STORAGE_KEY = 'vokabeltrainer_v07';
 const DB_NAME = 'vokabeltrainer-db';
 const DB_STORE = 'app-state';
