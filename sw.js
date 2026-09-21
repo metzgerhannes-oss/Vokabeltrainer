26158a07b77a0d8b0fb9a9c8595a5f333a592f29
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 11:30:13 +0200

    PWA: Family-Sync im Offline-Shell cachen

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION='0.17.3';
+const APP_VERSION='0.18.0';
 const RESOURCE_REVISION='1';
 const SHELL_CACHE=`vokabeltrainer-shell-v${APP_VERSION}`;
 const RESOURCE_CACHE=`vokabeltrainer-resources-v${RESOURCE_REVISION}`;
@@ -10,9 +10,9 @@ const LEGACY_CACHE_PREFIX='vokabeltrainer-v';
 const LEGACY_APP_BASES=['/JohannasGartenwelt/vokabeltrainer/'];
 
 const ASSETS=[
-  './','./index.html','./css/app.css?v=0.17.3','./js/core.js?v=0.17.3','./js/help.js?v=0.17.3','./js/library.js?v=0.17.3','./js/storage.js?v=0.17.3',
-  './js/model.js?v=0.17.3','./js/quiz-engine.js?v=0.17.3','./js/learning.js?v=0.17.3','./js/translation.js?v=0.17.3',
-  './js/io.js?v=0.17.3','./js/ui.js?v=0.17.3','./js/library-ui.js?v=0.17.3','./js/focus-ui.js?v=0.17.3','./js/app.js?v=0.17.3','./manifest.webmanifest',
+  './','./index.html','./css/app.css?v=0.18.0','./js/core.js?v=0.18.0','./js/help.js?v=0.18.0','./js/library.js?v=0.18.0','./js/storage.js?v=0.18.0',
+  './js/model.js?v=0.18.0','./js/quiz-engine.js?v=0.18.0','./js/learning.js?v=0.18.0','./js/translation.js?v=0.18.0',
+  './js/io.js?v=0.18.0','./js/family-sync.js?v=0.18.0','./js/ui.js?v=0.18.0','./js/library-ui.js?v=0.18.0','./js/focus-ui.js?v=0.18.0','./js/app.js?v=0.18.0','./manifest.webmanifest',
   './assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
 ];
 
