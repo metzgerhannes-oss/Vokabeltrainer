c71b480b0f9a772c364aaff5b36afef0fe15cc91
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 06:46:17 +0200

    Release v0.12.2: Service Worker aktualisieren

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.12.1')
+    navigator.serviceWorker.register('./sw.js?v=0.12.2')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
