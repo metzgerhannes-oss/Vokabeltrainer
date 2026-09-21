b0147e9d57ffa3140d325a759bd2276170cb1027
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 20:25:06 +0200

    Release v0.18.6 vorbereiten

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.18.5')
+    navigator.serviceWorker.register('./sw.js?v=0.18.6')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
