c32f0599e1da8706dd494f8156c1640f1e8fe35f
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 06:48:56 +0200

    Release v0.12.3: neuen Shell-Cache verwenden

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION='0.12.2';
+const APP_VERSION='0.12.3';
 const RESOURCE_REVISION='1';
 const SHELL_CACHE=`vokabeltrainer-shell-v${APP_VERSION}`;
 const RESOURCE_CACHE=`vokabeltrainer-resources-v${RESOURCE_REVISION}`;
@@ -10,9 +10,9 @@ const LEGACY_CACHE_PREFIX='vokabeltrainer-v';
 const LEGACY_APP_BASES=['/JohannasGartenwelt/vokabeltrainer/'];
 
 const ASSETS=[
-  './','./index.html','./css/app.css?v=0.12.2','./js/core.js?v=0.12.2','./js/help.js?v=0.12.2','./js/library.js?v=0.12.2','./js/storage.js?v=0.12.2',
-  './js/model.js?v=0.12.2','./js/quiz-engine.js?v=0.12.2','./js/learning.js?v=0.12.2','./js/translation.js?v=0.12.2',
-  './js/io.js?v=0.12.2','./js/ui.js?v=0.12.2','./js/library-ui.js?v=0.12.2','./js/focus-ui.js?v=0.12.2','./js/app.js?v=0.12.2','./manifest.webmanifest',
+  './','./index.html','./css/app.css?v=0.12.3','./js/core.js?v=0.12.3','./js/help.js?v=0.12.3','./js/library.js?v=0.12.3','./js/storage.js?v=0.12.3',
+  './js/model.js?v=0.12.3','./js/quiz-engine.js?v=0.12.3','./js/learning.js?v=0.12.3','./js/translation.js?v=0.12.3',
+  './js/io.js?v=0.12.3','./js/ui.js?v=0.12.3','./js/library-ui.js?v=0.12.3','./js/focus-ui.js?v=0.12.3','./js/app.js?v=0.12.3','./manifest.webmanifest',
   './assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
 ];
 
