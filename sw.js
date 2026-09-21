0b306afef2329965b99a77cb62be5a1db66c47bf
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-21 21:19:25 +0200

    Release v0.18.9 vorbereiten

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION='0.18.8';
+const APP_VERSION='0.18.9';
 const RESOURCE_REVISION='1';
 const SHELL_CACHE=`vokabeltrainer-shell-v${APP_VERSION}`;
 const RESOURCE_CACHE=`vokabeltrainer-resources-v${RESOURCE_REVISION}`;
@@ -10,9 +10,9 @@ const LEGACY_CACHE_PREFIX='vokabeltrainer-v';
 const LEGACY_APP_BASES=['/JohannasGartenwelt/vokabeltrainer/'];
 
 const ASSETS=[
-  './','./index.html','./css/app.css?v=0.18.8','./js/core.js?v=0.18.8','./js/help.js?v=0.18.8','./js/library.js?v=0.18.8','./js/storage.js?v=0.18.8',
-  './js/builtin-library.js?v=0.18.8','./js/model.js?v=0.18.8','./js/quiz-engine.js?v=0.18.8','./js/learning.js?v=0.18.8','./js/translation.js?v=0.18.8',
-  './js/io.js?v=0.18.8','./js/family-sync.js?v=0.18.8','./js/ui.js?v=0.18.8','./js/library-ui.js?v=0.18.8','./js/focus-ui.js?v=0.18.8','./js/app.js?v=0.18.8','./manifest.webmanifest',
+  './','./index.html','./css/app.css?v=0.18.9','./js/core.js?v=0.18.9','./js/help.js?v=0.18.9','./js/library.js?v=0.18.9','./js/storage.js?v=0.18.9',
+  './js/builtin-library.js?v=0.18.9','./js/model.js?v=0.18.9','./js/quiz-engine.js?v=0.18.9','./js/learning.js?v=0.18.9','./js/translation.js?v=0.18.9',
+  './js/io.js?v=0.18.9','./js/family-sync.js?v=0.18.9','./js/ui.js?v=0.18.9','./js/library-ui.js?v=0.18.9','./js/focus-ui.js?v=0.18.9','./js/app.js?v=0.18.9','./manifest.webmanifest',
   './data/camden-town-1-welcome.tsv','./data/camden-town-1-theme1.tsv',
   './assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
 ];
