b097baea9ad99b984c9097bb727a107ae8ba9564
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-22 21:02:33 +0200

    Bump v0.18.17 visual polish

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.18.16')
+    navigator.serviceWorker.register('./sw.js?v=0.18.17')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
