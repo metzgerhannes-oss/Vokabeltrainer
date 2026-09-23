2590a22ee768a1977b1650aedb1532ff9b4073ed
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-23 21:46:41 +0200

    Bump service worker registration v0.18.36

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.18.35')
+    navigator.serviceWorker.register('./sw.js?v=0.18.36')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
