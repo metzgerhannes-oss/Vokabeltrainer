9393f253ec6b70410bf377dee39af05a3811f493
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-23 13:51:17 +0200

    Bump app version for army artwork

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
       if(sessionStorage.getItem(key))return;
       updateReloading=true;sessionStorage.setItem(key,'1');location.reload();
     });
-    navigator.serviceWorker.register('./sw.js?v=0.18.20')
+    navigator.serviceWorker.register('./sw.js?v=0.18.21')
       .then(reg=>reg.update().catch(()=>{}))
       .catch(console.warn);
   }
