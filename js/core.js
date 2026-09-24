6242ed9074638686ea1160289e43a8825a09419b
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-24 19:57:34 +0200

    Bump js/core.js to v0.18.54

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '0.18.53';
+const VERSION = '0.18.54';
 const STORAGE_KEY = 'vokabeltrainer_v07';
 const DB_NAME = 'vokabeltrainer-db';
 const DB_STORE = 'app-state';
