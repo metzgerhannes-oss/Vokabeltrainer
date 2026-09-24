20be01864cb42ec806e733f045cac618a251634b
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-24 19:09:53 +0200

    Fix iOS 15 family pairing modal taps
    Fix child-device pairing modal interaction on iOS 15 while preserving existing family pairing behavior and regression coverage.

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '0.18.50';
+const VERSION = '0.18.51';
 const STORAGE_KEY = 'vokabeltrainer_v07';
 const DB_NAME = 'vokabeltrainer-db';
 const DB_STORE = 'app-state';
