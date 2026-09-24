f8347068950994b2630efbe6b3bd4a87542147bd
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-24 15:10:13 +0200

    Give army units clear roles and visible strengths
    * Define army unit roles and visible role strengths
    
    * Add army role strength section
    
    * Style army roles and strength breakdown
    
    * Test army roles and strength breakdown
    
    * Bump army roles release to 0.18.42
    
    * Bump army roles release to 0.18.42
    
    * Bump army roles release to 0.18.42
    
    * Bump army roles release to 0.18.42
    
    * Bump army roles release to 0.18.42
    
    * Bump army roles release to 0.18.42
    
    * Document army unit roles
    
    * Audit army role separation

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '0.18.41';
+const VERSION = '0.18.42';
 const STORAGE_KEY = 'vokabeltrainer_v07';
 const DB_NAME = 'vokabeltrainer-db';
 const DB_STORE = 'app-state';
