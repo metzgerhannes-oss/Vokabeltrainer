516bf0f29ec8d8993e0908f4d68ae431963af71c
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-24 15:21:57 +0200

    Bump tactical battle release to 0.18.44

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '0.18.43';
+const VERSION = '0.18.44';
 const STORAGE_KEY = 'vokabeltrainer_v07';
 const DB_NAME = 'vokabeltrainer-db';
 const DB_STORE = 'app-state';
